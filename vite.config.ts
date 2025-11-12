f5994c9b9008f0b3c01d6ebe4b2f82e165235b86
Moiri Gamboni <moiri.gamboni@proton.me>
2025-11-12 12:19:17 +0100

    Add Tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
+import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [
-		enhancedImages(),
-		sveltekit()
-	]
+	plugins: [tailwindcss(), enhancedImages(), sveltekit()]
 });
