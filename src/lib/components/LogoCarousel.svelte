7e935aaa225c9abe0d30e509e644b8d39827b77b
Moiri Gamboni <moiri.gamboni@proton.me>
2025-12-08 21:07:21 +0100

    Fix carousel breakpoints

--- src/lib/components/LogoCarousel.svelte
+++ src/lib/components/LogoCarousel.svelte
@@ -64,11 +64,11 @@
 				</Carousel.Item>
 			{/each}
 		</Carousel.Content>
-		<div class="mt-6 flex justify-center gap-4 md:hidden">
+		<div class="mt-6 flex justify-center gap-4 xl:hidden">
 			<Carousel.Previous class="relative left-auto top-auto translate-y-0 border-green-500/30 bg-white hover:border-green-500 hover:bg-green-500/10" />
 			<Carousel.Next class="relative right-auto top-auto translate-y-0 border-green-500/30 bg-white hover:border-green-500 hover:bg-green-500/10" />
 		</div>
-		<Carousel.Previous class="hidden border-green-500/30 bg-white hover:border-green-500 hover:bg-green-500/10 md:flex" />
-		<Carousel.Next class="hidden border-green-500/30 bg-white hover:border-green-500 hover:bg-green-500/10 md:flex" />
+		<Carousel.Previous class="hidden border-green-500/30 bg-white hover:border-green-500 hover:bg-green-500/10 xl:flex" />
+		<Carousel.Next class="hidden border-green-500/30 bg-white hover:border-green-500 hover:bg-green-500/10 xl:flex" />
 	</Carousel.Root>
 </div>
